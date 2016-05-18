5fae3f4798d0c3efa2a231b577ead5a55463a38e
Thomas Walker <thomasbwalker@gmail.com>
2016-05-18 17:58:46 -0500

    update 1

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     alert("Welcome to Thomas Walker's Online Resume!");
 });    
 
-var colors = document.getElementById('color');
-colors.addEventListener('click', randomColor);
+var colors = document.getElementById('container');
+colors.addEventListener('mouseenter', randomColor);
+console.log(colors);
 
 function randomColor(e){
     var letters = '0123456789ABCDEF'.split('');
@@ -14,6 +15,6 @@ function randomColor(e){
     e.target.style.color = color;
 };
 
-$('#Education').on('click', function (){
-    $('#animate').animate({left: '+=50px'}, 10000);
+$('#Education').click(function (){
+    $('#animate').animate({left: '+=30em'}, 10000);
 })
